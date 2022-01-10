06d6dda222d83e1b358087607dcddf0a9c63f6dc
Cuong Ngo <cuongngo@u.nus.edu>
2022-01-10 23:17:51 +0800

    Successully transfer initial DAI token in deploy script

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,5 +1,5 @@
 from scripts.helpful_scripts import get_account
-from brownie import PotamusLoan
+from brownie import PotamusLoan, interface
 from web3 import Web3
 import yaml
 import json
@@ -10,6 +10,19 @@ import shutil
 def deploy_token_farm_and_dapp_token(front_end_update=False):
     account = get_account()
     potamus_loan = PotamusLoan.deploy({"from": account})
+
+    dai = interface.IERC20('0x04DF6e4121c27713ED22341E7c7Df330F56f289B')
+    
+    tx = dai.approve(potamus_loan.address, 10 * 10 ** 18, {"from": account})
+    tx.wait(1)
+
+    tx = potamus_loan.deposit(
+        '0x04DF6e4121c27713ED22341E7c7Df330F56f289B', #DAI Token address on Kovan testnet
+        3 * 10 ** 18,
+        {"from": account},
+    )
+    tx.wait(1)
+
     if front_end_update:
         update_front_end()
     return potamus_loan
